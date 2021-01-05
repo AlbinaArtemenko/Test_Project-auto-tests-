077171fac3bf9debd03dc7ff58bbe961e0f6c0e8
AlbinaArtemenko <albina@wizardsdev.com>
2021-01-05 17:25:34 +0200

    last changes in the firstteste2e

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -5,8 +5,21 @@ describe('Example', () => {
 
   
 
-  it('should show world screen after tap', async () => {
-    // await element(by.id('inputService˝RegistrationView')).tap();
-    await expect(element(by.id('testDetox'))).toBeVisible();
+  // it('should show world screen after tap', async () => {
+  //   // await element(by.id('inputService˝RegistrationView')).tap();
+  //   await expect(element(by.id('testDetox'))).toBeVisible();
+  // });
+  
+  it('view save',  async()=> {
+    await expect(element(by.id('buttonRegistrateRegistrationView'))).toBeVisible();
+    });
+
+  it('RegistrationView', async()=> {
+    await element(by.id('inputService˝RegistrationView')).typeText("albina");
+    await element(by.id('inputPassworRegistrationView')).typeText("1234567");
+    await element(by.id('inputPinRegistrationView')).typeText("1111111");
+    await element(by.id('inputLoginRegistrationView')).typeText("380678282323");
+    await element(by.id('inputImailRegistrationView')).typeText("albina@gmail.com");
+    await element(by.id('buttonRegistrateRegistrationView')).tap();
   });
-});
+  });
